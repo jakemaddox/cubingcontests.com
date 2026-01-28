2eda9c2e3058281773244b0cf1201a96b9adeeb5
Deni <cube327@tuta.io>
2026-01-28 12:38:28 +0100

    fix: revert start-prod.sh changes

--- bin/start-prod.sh
+++ bin/start-prod.sh
@@ -6,33 +6,33 @@
 
 # $1 - (optional) --restart/-r - skip apt update and DB dump
 
-if [ "$(pwd | tail -c 5)" == "/bin" ] || [ $EUID != 0 ]; then
-  echo "Please run this script with sudo from the repo's root directory"
+if [ "$(pwd | tail -c 5)" == "/bin" ]; then
+  echo "Please run this script from the repo's root directory"
   exit 1
 fi
 
-source .env
-docker pull "$DOCKER_IMAGE_NAME"
+sudo source .env
+sudo docker pull "$DOCKER_IMAGE_NAME"
 
 if [ "$1" != "--restart" ] && [ "$1" != "-r" ]; then
   cd client &&
   pnpm run db:migrate &&
   cd .. &&
 
-  docker compose -f docker-compose.cc.yml up -d
+  sudo docker compose -f docker-compose.cc.yml up -d
 else
-  apt update &&
-  apt dist-upgrade &&
+  sudo apt update &&
+  sudo apt dist-upgrade &&
 
   # TO-DO: MAKE DUMPS WORK AGAIN (use Supabase cron)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   # ./bin/dump-db.sh /dump
 
-  docker stop cc-nextjs &&
+  sudo docker stop cc-nextjs &&
 
   cd client &&
   pnpm run db:migrate &&
   cd .. &&
   echo && # just print a new line in the terminal
 
-  docker compose -f docker-compose.cc.yml up -d
+  sudo docker compose -f docker-compose.cc.yml up -d
 fi
